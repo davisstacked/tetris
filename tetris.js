12f50b1dd5f2eb567b1b54a57a537363a5f90846
davisstacked <brettzvi@gmail.com>
2021-01-20 11:16:00 -0800

    multiple pieces and collisions

--- tetris.js
+++ tetris.js
@@ -9,6 +9,28 @@ const matrix = [
     [0, 0, 0],
 ];
 
+function collide(arena, player) {
+    const m = player.matrix;
+    const o = player.pos;
+    for(let y = 0; y < m.length; y++) {
+        for(let x=0; x < m[y].length; x++) {
+            if(m[y][x] !== 0 && 
+            (arena[y + o.y] && arena[y + o.y][x + o.x]) !== 0) {
+                return true;
+            }
+        } 
+    }
+    return false;
+}
+
+function createMatrix(w, h) {
+    const matrix = [];
+    while(h--) {
+        matrix.push(new Array(w).fill(0));
+    }
+    return matrix;
+}
+
 function drawMatrix(matrix, offset) {
     matrix.forEach((row, y) => {
         row.forEach((value, x) => {
@@ -20,6 +42,9 @@ function drawMatrix(matrix, offset) {
     });
 };
 
+
+const arena = createMatrix(12, 20);
+
 const player = {
     pos: {x: 5, y: 3},
     matrix: matrix,
@@ -29,11 +54,27 @@ function draw() {
     context.fillStyle = "#000";
     context.fillRect(0,0, canvas.width, canvas.height);
 
+    drawMatrix(arena, {x: 0, y: 0});
     drawMatrix(player.matrix, player.pos);
 }
 
+function merge(arena, player) {
+    player.matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if(value !== 0) {
+                arena[y + player.pos.y] [ x + player.pos.x] = value;
+            }
+        });
+    });
+};
+
 function playerDrop() {
     player.pos.y++;
+    if(collide(arena, player)) {
+        player.pos.y--;
+        merge(arena, player);
+        player.pos.y = 0;
+    }
     dropCounter = 0;
 }
 
