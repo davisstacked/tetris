37edde56c8e32990f7ef5a1f4ddb6736b71af615
davisstacked <brettzvi@gmail.com>
2021-01-20 10:44:04 -0800

    piece moves

--- tetris.js
+++ tetris.js
@@ -1,8 +1,6 @@
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
 
-context.fillStyle = "#000";
-context.fillRect(0,0, canvas.width, canvas.height);
 context.scale(20,20);
 
 const matrix = [
@@ -22,4 +20,36 @@ function drawMatrix(matrix, offset) {
     });
 };
 
-drawMatrix(matrix, {x: 5, y:3});
+const player = {
+    pos: {x: 5, y: 3},
+    matrix: matrix,
+};
+
+function draw() {
+    context.fillStyle = "#000";
+    context.fillRect(0,0, canvas.width, canvas.height);
+
+    drawMatrix(player.matrix, player.pos);
+}
+
+let dropCounter = 0
+let dropInterval = 1000;
+
+let lastTime = 0;
+
+function update(time = 0) {
+    const deltaTime = time - lastTime;
+
+    dropCounter += deltaTime;
+    if(dropCounter > dropInterval) {
+        player.pos.y++;
+        dropCounter = 0;
+    }
+
+    lastTime = time;
+    draw();
+
+    requestAnimationFrame(update);
+}
+
+update();
