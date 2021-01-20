91a3739a380aae9adab776040e23a36408d833a7
davisstacked <brettzvi@gmail.com>
2021-01-20 11:34:16 -0800

    rotate pieces - collide with sides of screen

--- tetris.js
+++ tetris.js
@@ -68,6 +68,27 @@ function merge(arena, player) {
     });
 };
 
+
+function rotate(matrix, dir) {
+    for(let y = 0; y < matrix.length; y++) {
+        for(let x = 0; x < y; x++) {
+            [
+                matrix[x][y],
+                matrix[y][x],
+            ] = [
+                matrix[y][x],
+                matrix[x][y],
+            ];
+        }
+    }
+    if (dir > 0) {
+        matrix.forEach(row => row.reverse());
+    } else {
+        matrix.reverse();
+    }
+}
+
+
 function playerDrop() {
     player.pos.y++;
     if(collide(arena, player)) {
@@ -77,6 +98,34 @@ function playerDrop() {
     }
     dropCounter = 0;
 }
+
+
+function playerMove(offset) {
+    player.pos.x += offset;
+    if(collide(arena, player)) {
+        player.pos.x -= offset;
+    }
+}
+
+
+function playerRotate(dir) {
+    const pos = player.pos.x;
+    let offset = 1;
+    rotate(player.matrix, dir);
+
+    while(collide(arena, player)) {
+        player.pos.x += offset;
+        offset = -(offset + (offset > 0 ? 1: -1));
+        if(offset > player.matrix[0].length) {
+            rotate(player.matrix, -dir);
+            player.pos.x = pos;
+            return;
+        }
+    }
+}
+
+
+
 
 let dropCounter = 0
 let dropInterval = 1000;
@@ -99,11 +148,15 @@ function update(time = 0) {
 
 document.addEventListener('keydown', event => {
     if(event.keyCode === 37) {
-        player.pos.x--;
+        playerMove(-1);
     } else if (event.keyCode === 39) {
-        player.pos.x++;
+        playerMove(1);
     } else if (event.keyCode === 40) {
         playerDrop();
+    } else if (event.keyCode === 81) {
+        playerRotate(-1);
+    } else if (event.keyCode === 87) {
+        playerRotate(1);
     }
 })
 
