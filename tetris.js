69b16b0394909dc5a0c2fbfecc2f78d57b89cd16
davisstacked <brettzvi@gmail.com>
2021-01-20 10:33:15 -0800

    added a red piece on the screen

--- tetris.js
+++ tetris.js
@@ -3,3 +3,23 @@ const context = canvas.getContext('2d');
 
 context.fillStyle = "#000";
 context.fillRect(0,0, canvas.width, canvas.height);
+context.scale(20,20);
+
+const matrix = [
+    [0, 1, 0],
+    [1, 1, 1],
+    [0, 0, 0],
+];
+
+function drawMatrix(matrix, offset) {
+    matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if(value !== 0 ) {
+                context.fillStyle = "red";
+                context.fillRect(x + offset.x, y + offset.y, 1, 1);
+            }
+        });
+    });
+};
+
+drawMatrix(matrix, {x: 5, y:3});
